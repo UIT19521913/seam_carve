34953188aafae147e0505d09e17dbfb8728072cd
Nguyen Nguyen <19521913@gm.uit.edu.vn>
2021-11-28 20:12:28 +0700

    Update UI.py

--- UI.py
+++ UI.py
@@ -24,7 +24,7 @@ img_input = st.sidebar.file_uploader("Upload a photo", type=["png", "jpg"])
 
 nav = st.sidebar.selectbox("Chọn thao tác muốn thực hiện", [
                            "Resize", "Remove object"])
-
+dst = None
 if img_input is not None:
     # Convert the file to an opencv image.
     file_bytes = np.asarray(bytearray(img_input.read()), dtype=np.uint8)
@@ -38,11 +38,46 @@ if img_input is not None:
             "Nhập chiều cao", value=src_h, format="%d", step=2)
         width = st.sidebar.number_input(
             "Nhập chiều rộng", value=src_w, format="%d", step=2)
+       
+        energy = st.sidebar.selectbox("Chọn phương pháp tìm đường seam", [
+                           "backward", "forward"])
+        order = st.sidebar.selectbox("Chọn thứ tự thêm/ xoá đường seam", [
+                            "width-first", "height-first"])
+        
+        keep_mask_select = st.sidebar.checkbox("Chọn đối tượng giữ ")
+        keep_mask = None
+        if keep_mask_select:
+            st.write("Keep mask")
+            canvas_keep = st_canvas(
+                # Fixed fill color with some opacity
+                fill_color="rgba(255, 255, 255, .3)",
+                stroke_width=1,
+                stroke_color="#000000",
+                background_color="#000000",
+                background_image=Image.open(img_input) if img_input else None,
+                update_streamlit=False,
+                height=src_h,
+                width=src_w,
+                drawing_mode="polygon",
+                display_toolbar=True,
+                key="full_app_2",
+            )
+
+            if canvas_keep.image_data is not None and canvas_keep.image_data.any():
+                keep_mask = np.array(
+                    canvas_keep.image_data[:, :, :3], dtype=np.uint8)
+                keep_mask = (keep_mask[:, :, 0] > 10) * 255
+                st.image(keep_mask)
+
         button = st.sidebar.button("save")
         if button:
-            st.write("Kết quả")
-            dst = seam_carving.resize(opencv_image, (width, height))
+            status = st.empty()
+            start = time.time()
+            status.write("Performing seam carving...")
+            dst = seam_carving.resize(opencv_image, (width, height), energy, order, keep_mask)
+            status.write('Done at {:.4f} second(s)'.format(time.time() - start))
             st.image(dst, channels="BGR")
+        
 
     if nav == "Remove object":
         drop_mask_select = st.sidebar.checkbox("Chọn đối tượng cần xoá", True)
@@ -74,7 +109,6 @@ if img_input is not None:
         keep_mask = None
         if keep_mask_select:
             st.write("Keep mask")
-            st.write("Drop Mask")
             canvas_keep = st_canvas(
                 # Fixed fill color with some opacity
                 fill_color="rgba(255, 255, 255, .3)",
@@ -96,6 +130,7 @@ if img_input is not None:
                 keep_mask = (keep_mask[:, :, 0] > 10) * 255
                 st.image(keep_mask)
 
+        size_unchange = st.sidebar.checkbox("Không thay đổi kích thước ảnh")
         confirm = st.button("Confirm")
         if confirm:
             status = st.empty()
@@ -104,6 +139,8 @@ if img_input is not None:
 
             dst = seam_carving.remove_object(
                 opencv_image, drop_mask, keep_mask)
+            if size_unchange:
+                dst = seam_carving.resize(dst, (src_w, src_h))
             status.write('Done at {:.4f} second(s)'.format(
                 time.time() - start))
             st.image(dst, channels="BGR")
